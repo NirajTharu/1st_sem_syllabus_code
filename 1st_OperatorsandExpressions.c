4661f7afab9a00c7dbdead279fb49716383267d4
NirajTharu <nirajc279@gmail.com>
2024-01-23 18:50:15 +0545

    6:49PM
    1st_sem

---
+++ 1st_OperatorsandExpressions.c
@@ -0,0 +1,11 @@
+/* Program to understand the integer arithmetic operation*/
+#include<stdio.h>
+int main ()
+{
+	int a = 17, b = 4;
+	printf ("Sum = %d\n", a +b );
+	printf ("Difference = %d\n", a - b);
+	printf ("Product = %d\n", a * b);
+	printf ("Quotient = %d\n", a / b);
+	printf ("Remainder = %d\n", a % b);
+}
